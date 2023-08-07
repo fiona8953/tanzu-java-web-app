ba8d7f479b3245928884d9135a7ee6a0ee2501b6
Fiona Zou <38024126+fiona8953@users.noreply.github.com>
2023-08-07 21:39:31 +0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! test5";
+		return "Greetings from Spring Boot + Tanzu! test";
 	}
 
 }
