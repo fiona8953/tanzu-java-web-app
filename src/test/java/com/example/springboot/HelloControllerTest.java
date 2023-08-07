13acae1201e77204abfc13189325a24aaa229fd8
Fiona Zou <38024126+fiona8953@users.noreply.github.com>
2023-08-07 21:39:16 +0800

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu! test5", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu! test", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu! test5"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu! test"));
     }
 }
